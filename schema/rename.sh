b959d8dd23e9f42ec4c1bdd0eae1fa8cdcd56983
guo-shaoge <shaoge1994@163.com>
2023-07-03 22:42:25 +0800

    fix
    Signed-off-by: guo-shaoge <shaoge1994@163.com>

--- schema/rename.sh
+++ schema/rename.sh
@@ -7,12 +7,12 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-mv $1.customer-schema.sql $2.customer-schema.sql
-mv $1.lineitem-schema.sql $2.lineitem-schema.sql
-mv $1.nation-schema.sql   $2.nation-schema.sql
-mv $1.orders-schema.sql   $2.orders-schema.sql
-mv $1.part-schema.sql     $2.part-schema.sql
-mv $1.partsupp-schema.sql $2.partsupp-schema.sql
-mv $1.region-schema.sql   $2.region-schema.sql
-mv $1-schema-create.sql   $2-schema-create.sql
-mv $1.supplier-schema.sql $2.supplier-schema.sql
+mv tpch$1.customer-schema.sql tpch$2.customer-schema.sql
+mv tpch$1.lineitem-schema.sql tpch$2.lineitem-schema.sql
+mv tpch$1.nation-schema.sql   tpch$2.nation-schema.sql
+mv tpch$1.orders-schema.sql   tpch$2.orders-schema.sql
+mv tpch$1.part-schema.sql     tpch$2.part-schema.sql
+mv tpch$1.partsupp-schema.sql tpch$2.partsupp-schema.sql
+mv tpch$1.region-schema.sql   tpch$2.region-schema.sql
+mv tpch$1-schema-create.sql   tpch$2-schema-create.sql
+mv tpch$1.supplier-schema.sql tpch$2.supplier-schema.sql
